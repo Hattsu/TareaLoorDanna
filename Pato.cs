b4af19f407b1ad276433c89ae97144e223a1cd5b
Hattsu <102260446+Hattsu@users.noreply.github.com>
2022-06-09 23:45:02 -0500

    Update Pato.cs

--- Pato.cs
+++ Pato.cs
@@ -4,28 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ave
+namespace Ejercicios_aves
 {
-    public class Pato : Ave, iAve
+    public class Pato : Aves, IAve
 
     {
         public Pato(string nombre, string color) : base(nombre, color) { }
 
-        public void nadar_AVE()
+        public void Nadar_ave()
         {
             Console.WriteLine("-------------------");
             Console.WriteLine("");
-            Console.WriteLine("El nombre es " + nombre);
-            Console.WriteLine("El color es " + color);
+            Console.WriteLine("El nombre es " + Nombre);
+            Console.WriteLine("El color es " + Color);
             Console.WriteLine("");
             Console.WriteLine("-------------------");
 
         }
-        public void volar_AVE()
+        public void Volar_ave()
         {
             Console.WriteLine("-------------------");
             Console.WriteLine("");
-            Console.WriteLine("Pato Donalt" + nombre);
+            Console.WriteLine("Pato Donalt" + Nombre);
             Console.WriteLine("I BELIVE I CAN FLY");
             Console.WriteLine("");
             Console.WriteLine("-------------------");
